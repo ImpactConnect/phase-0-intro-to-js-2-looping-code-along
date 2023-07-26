1b35fdc0e79a4bf0295d965cd4ae7198ebda6f0e
Omotayo <kehindeomotayo11@gmail.com>
2023-07-26 14:12:15 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+// const names = ["Guadalupe,", "Ollie,", "Aki"];
+
+// function writeCards() {
+//   let name = 0;
+//   while (name < names.length) {
+//     console.log(`Thank you, ${names[name]} for the wonderful surprise gift!`);
+//     name++;
+//   }
+//   return names;
+// }
+// writeCards();
+
+function writeCards(names) {
+  const thankYouMessages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]} for the wonderful surprise gift!`;
+    thankYouMessages.push(message);
+  }
+
+  return thankYouMessages;
+}
+
+const namesArray = ["Guadalupe,", "Ollie,", "Aki"];
+const messages = writeCards(namesArray);
+console.log(messages);
+//=======
+
+function countDown(num) {
+  for (let i = num; i >= 0; i--) {
+    console.log(i);
+  }
+}
+countDown(10);
